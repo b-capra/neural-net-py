b5d5f673801e188db64866d6f7e7384a7672fec9
b-capra <caprabmc@gmail.com>
2023-01-16 15:18:42 -0500

    Refactor layer function with zipped lists

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ weights = [[0.2, 0.8, -0.5, 1.0],
            [-0.26, -0.27, 0.17, 0.87]]
 biases = [2, 3, 0.5]
 
-outputs = []
-for row in weights:
-  output = 0
-  for wght in row:
-    output += inputs[row.index(wght)] * wght
-  output += biases[weights.index(row)]
-  outputs.insert(weights.index(row), output)
+layer_outputs = []
+for neuron_weights, neuron_bias in zip(weights, biases):
+  neuron_output = 0
+  for n_input, weight in zip(inputs, neuron_weights):
+    neuron_output += n_input*weight
+  neuron_output += neuron_bias
+  layer_outputs.append(neuron_output)
   
-print(outputs)
-
+print(layer_outputs)
